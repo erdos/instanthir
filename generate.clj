2b1fcc2dd998c1a56c3468e7bd6db522367e5ef2
janos erdos <erdosj@gmail.com>
2020-08-31 21:54:26 +0200

    wip

--- generate.clj
+++ generate.clj
@@ -14,15 +14,48 @@
      :url url
      :tags (vec categories)}))
 
-(->
- "https://24.hu/feed/"
- (curl/get)
- :body
- (xml/parse-str)
- :content
- (->> (some :content))
- (->> (filter (comp #{:item} :tag)))
- (->> (map rss-item))
- (pr-str)
+(defn fetch-items [url]
+  (->
+   url
+   (curl/get)
+   :body
+   (xml/parse-str)
+   :content
+   (->> (some :content))
+   (->> (filter (comp #{:item} :tag)))
+   (->> (map rss-item))))
 
- (->> (spit "docs/index.html")))
+(defn render-html [elem]
+  (cond
+    (vector? elem)
+    (str "<" (name (first elem)) ">" (apply str (map render-html (next elem))) "</" (name (first elem)) ">")
+
+    (sequential? elem)
+    (apply str (map render-html elem))
+
+    :else
+    (str elem)))
+
+(defn template [items]
+  [:html
+   [:body
+    [:h1 "Hello"]
+    [:table
+     (for [item items]
+       [:tr
+        [:td (:title item)]
+        ]
+       )]
+    ]])
+
+
+
+(->
+ (concat
+  (fetch-items "https://24.hu/feed/")
+  (fetch-items "https://444.hu/feed/")
+  (fetch-items "https://hvg.hu/rss"))
+ (template)
+ (render-html)
+ (->> (spit "docs/index.html"))
+ )
