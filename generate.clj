81006579301f6d2f810493cf1ddd71e97eedd94a
janos erdos <erdosj@gmail.com>
2020-08-31 21:38:31 +0200

    asdf

--- generate.clj
+++ generate.clj
@@ -1,1 +1,28 @@
-(spit "docs/index.html" (pr-str *ns*))
+(require '[babashka.classpath :refer [add-classpath]]
+         '[clojure.string :as str]
+         '[babashka.curl :as curl]
+         )
+
+(defn rss-item [item]
+  (let [title (some #(when (= :title (:tag %)) (first (:content %)))
+                    (:content item))
+        url   (some #(when (= :link (:tag %)) (first (:content %)))
+                    (:content item))
+        categories (keep #(when (= :category (:tag %)) (first (:content %)))
+                          (:content item))]
+    {:title title
+     :url url
+     :tags (vec categories)}))
+
+(->
+ "https://24.hu/feed/"
+ (curl/get)
+ :body
+ (xml/parse-str)
+ :content
+ (->> (some :content))
+ (->> (filter (comp #{:item} :tag)))
+ (->> (map rss-item))
+ (pr-str)
+
+ (->> (spit "docs/index.html")))
